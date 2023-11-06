159549ac1fb86924748fb7011e4df70433169d6e
erhansennx <erhanfatih33@gmail.com>
2023-11-06 15:57:43 +0300

    Navigation Graph created.

--- app/src/main/java/com/app/ebfitapp/view/AuthenticationActivity.kt
+++ app/src/main/java/com/app/ebfitapp/view/AuthenticationActivity.kt
@@ -2,11 +2,21 @@ package com.app.ebfitapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.app.ebfitapp.R
+import com.app.ebfitapp.databinding.ActivityAuthenticationBinding
 
 class AuthenticationActivity : AppCompatActivity() {
+
+    private lateinit var authenticationBinding: ActivityAuthenticationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+        authenticationBinding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(authenticationBinding.root)
+
+
     }
+
 }
