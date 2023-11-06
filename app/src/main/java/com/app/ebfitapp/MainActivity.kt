0780e0ae2337108eba4c88ac87914f8dc3816f26
erhansennx <erhanfatih33@gmail.com>
2023-11-06 12:44:39 +0300

    ViewBinding implemented.

--- app/src/main/java/com/app/ebfitapp/MainActivity.kt
+++ app/src/main/java/com/app/ebfitapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.app.ebfitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.ebfitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
+
 }
