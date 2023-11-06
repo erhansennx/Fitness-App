844df12567dc0d6b2e18ec8e081080ef4b1a3182
erhansennx <erhanfatih33@gmail.com>
2023-11-06 17:38:21 +0300

    Completed SignUpFragment screen design.

--- app/src/main/java/com/app/ebfitapp/fragment/SignUpFragment.kt
+++ app/src/main/java/com/app/ebfitapp/fragment/SignUpFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.app.ebfitapp.R
+import com.app.ebfitapp.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var fragmentSignUpBinding: FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fragmentSignUpBinding = FragmentSignUpBinding.inflate(layoutInflater)
 
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        val genderItems = listOf(getString(R.string.male), getString(R.string.female), getString(R.string.prefer_not_to_say))
+        val adapter = ArrayAdapter(requireContext(), R.layout.gender_items, genderItems)
+        fragmentSignUpBinding.genderDropDown.setAdapter(adapter)
+
+        return fragmentSignUpBinding.root
     }
 
 }
