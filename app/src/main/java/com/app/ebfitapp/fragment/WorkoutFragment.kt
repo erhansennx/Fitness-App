4bd81eb5fd5969043b1e4a299979217f54e95f97
erhansennx <erhanfatih33@gmail.com>
2023-11-15 00:12:32 +0300

    WorkoutFragment created, connection with navigation provided.

---
+++ app/src/main/java/com/app/ebfitapp/fragment/WorkoutFragment.kt
@@ -0,0 +1,20 @@
+package com.app.ebfitapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.ebfitapp.R
+
+class WorkoutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout, container, false)
+    }
+
+}
