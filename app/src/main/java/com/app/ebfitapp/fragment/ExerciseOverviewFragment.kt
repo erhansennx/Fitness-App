9c910f2385a3634263f4deaa3a9fff9f1a65b604
erhansennx <erhanfatih33@gmail.com>
2023-12-04 20:33:23 +0300

    WorkoutFragment reloading itself solved.

--- app/src/main/java/com/app/ebfitapp/fragment/ExerciseOverviewFragment.kt
+++ app/src/main/java/com/app/ebfitapp/fragment/ExerciseOverviewFragment.kt
@@ -7,13 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import com.app.ebfitapp.R
 import com.app.ebfitapp.databinding.FragmentExerciseOverviewBinding
+import com.app.ebfitapp.model.BodyPartExercisesItem
 
 class ExerciseOverviewFragment : Fragment() {
 
 
     private lateinit var exerciseOverviewBinding: FragmentExerciseOverviewBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         exerciseOverviewBinding = FragmentExerciseOverviewBinding.inflate(layoutInflater)
+
+        arguments.let {
+
+            val exercise = it!!.getSerializable("exercise") as BodyPartExercisesItem
+
+
+
+        }
 
 
 
--- app/src/main/java/com/app/ebfitapp/viewmodel/WorkoutViewModel.kt
+++ app/src/main/java/com/app/ebfitapp/viewmodel/WorkoutViewModel.kt
@@ -21,58 +21,63 @@ class WorkoutViewModel(private val application: Application) : AndroidViewModel(
     private val firestoreService = FirebaseFirestoreService(application.applicationContext)
 
     fun getMuscleGroups() {
-        firestoreService.firestore.collection("muscleGroups").get().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                for (snapshot in task.result.documents) {
-                    val muscleGroupModel = MuscleGroupModel(snapshot.data!!["muscle"].toString(), snapshot.data!!["muscleImage"].toString())
-                    muscleTempList.add(muscleGroupModel)
+        if (muscleGroupLiveData.value == null) {
+            firestoreService.firestore.collection("muscleGroups").get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (snapshot in task.result.documents) {
+                        val muscleGroupModel = MuscleGroupModel(snapshot.data!!["muscle"].toString(), snapshot.data!!["muscleImage"].toString())
+                        muscleTempList.add(muscleGroupModel)
+                    }
+                    muscleGroupLiveData.value = muscleTempList
+                } else {
+                    muscleGroupLiveData.value = null
                 }
-                muscleGroupLiveData.value = muscleTempList
-                muscleTempList.clear()
-            } else {
+            }.addOnFailureListener {
                 muscleGroupLiveData.value = null
+                showErrorToastMessage(it.message.toString())
             }
-        }.addOnFailureListener {
-            muscleGroupLiveData.value = null
-            showErrorToastMessage(it.message.toString())
         }
 
     }
 
     fun getBestTrainers() {
-        firestoreService.firestore.collection("bestTrainers").get().addOnCompleteListener {  query ->
-            if (query.isSuccessful) {
-                for (snapshot in query.result.documents) {
-                    val bestTrainersModel = BestTrainersModel(snapshot.data!!["profileImageURL"].toString(), snapshot.data!!["username"].toString(), snapshot.data!!["specialization"].toString())
-                    trainersTempList.add(bestTrainersModel)
-                }
-                bestTrainersLiveData.value = trainersTempList
-                trainersTempList.clear()
-            } else bestTrainersLiveData.value = null
+        if (bestTrainersLiveData.value == null) {
+            firestoreService.firestore.collection("bestTrainers").get().addOnCompleteListener {  query ->
+                if (query.isSuccessful) {
+                    for (snapshot in query.result.documents) {
+                        val bestTrainersModel = BestTrainersModel(snapshot.data!!["profileImageURL"].toString(), snapshot.data!!["username"].toString(), snapshot.data!!["specialization"].toString())
+                        trainersTempList.add(bestTrainersModel)
+                    }
+                    bestTrainersLiveData.value = trainersTempList
+                    //trainersTempList.clear()
+                } else bestTrainersLiveData.value = null
 
-        }.addOnFailureListener {
-            bestTrainersLiveData.value = null
-            showErrorToastMessage(it.message.toString())
+            }.addOnFailureListener {
+                bestTrainersLiveData.value = null
+                showErrorToastMessage(it.message.toString())
+            }
         }
     }
 
     fun getPopularWorkouts() {
-        firestoreService.firestore.collection("popularWorkouts").get().addOnCompleteListener { querySnapshot ->
-            if (querySnapshot.isSuccessful) {
-                for (snapshot in querySnapshot.result.documents) {
-                    val popularWorkouts = PopularWorkoutsModel(
-                        snapshot.data!!["name"].toString(),
-                        snapshot.data!!["description"].toString(),
-                        snapshot.data!!["imageURL"].toString()
-                    )
-                    workoutsTempList.add(popularWorkouts)
-                }
-                popularWorkoutsLiveData.value = workoutsTempList
-                workoutsTempList.clear()
-            } else popularWorkoutsLiveData.value = null
-        }.addOnFailureListener {
-            popularWorkoutsLiveData.value = null
-            showErrorToastMessage(it.message.toString())
+        if (popularWorkoutsLiveData.value == null) {
+            firestoreService.firestore.collection("popularWorkouts").get().addOnCompleteListener { querySnapshot ->
+                if (querySnapshot.isSuccessful) {
+                    for (snapshot in querySnapshot.result.documents) {
+                        val popularWorkouts = PopularWorkoutsModel(
+                            snapshot.data!!["name"].toString(),
+                            snapshot.data!!["description"].toString(),
+                            snapshot.data!!["imageURL"].toString()
+                        )
+                        workoutsTempList.add(popularWorkouts)
+                    }
+                    popularWorkoutsLiveData.value = workoutsTempList
+                    //workoutsTempList.clear()
+                } else popularWorkoutsLiveData.value = null
+            }.addOnFailureListener {
+                popularWorkoutsLiveData.value = null
+                showErrorToastMessage(it.message.toString())
+            }
         }
     }
 
