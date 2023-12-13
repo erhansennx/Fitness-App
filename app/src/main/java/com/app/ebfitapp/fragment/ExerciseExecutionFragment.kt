471d3af334cc882675c1fdf4d5cf53a128c69c4d
erhansennx <erhanfatih33@gmail.com>
2023-12-13 18:27:38 +0300

    Bottom sheet created in ExerciseExecutionFragment

--- app/src/main/java/com/app/ebfitapp/fragment/ExerciseExecutionFragment.kt
+++ app/src/main/java/com/app/ebfitapp/fragment/ExerciseExecutionFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.app.ebfitapp.R
 import com.app.ebfitapp.databinding.FragmentExerciseExecutionBinding
 import com.app.ebfitapp.model.BodyPartExercisesItem
 import com.app.ebfitapp.utils.downloadGifFromURL
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.textfield.TextInputEditText
 
 class ExerciseExecutionFragment : Fragment() {
 
@@ -27,8 +31,52 @@ class ExerciseExecutionFragment : Fragment() {
 
         }
 
+        showBottomSheet()
+
 
         return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.settings.setOnClickListener {
+            showBottomSheet()
+        }
+
+    }
+
+
+    private fun showBottomSheet() = with(binding) {
+        val bottomSheetDialog = BottomSheetDialog(requireContext())
+        val view = layoutInflater.inflate(R.layout.bottom_sheet_execution, null)
+        bottomSheetDialog.setContentView(view)
+        bottomSheetDialog.setCanceledOnTouchOutside(false)
+
+        val weightEditText = view.findViewById<TextInputEditText>(R.id.weightText)
+        val setsEditText = view.findViewById<TextInputEditText>(R.id.setsText)
+        val repsEditText = view.findViewById<TextInputEditText>(R.id.repsText)
+        val saveButton = view.findViewById<Button>(R.id.saveButton)
+
+        saveButton.setOnClickListener {
+            val enteredWeight = weightEditText.text.toString()
+            val enteredSets = setsEditText.text.toString()
+            val enteredReps = repsEditText.text.toString()
+
+            if (enteredWeight.isEmpty() || enteredSets.isEmpty() || enteredReps.isEmpty()) {
+                Toast.makeText(requireContext(), getString(R.string.please_fill_in_the_empty_fields), Toast.LENGTH_SHORT).show()
+            } else {
+                selectedWeight.text = enteredWeight
+                selectedSets.text = enteredSets
+                selectedReps.text = enteredReps
+                bottomSheetDialog.dismiss()
+            }
+
+        }
+
+        bottomSheetDialog.show()
     }
 
 
