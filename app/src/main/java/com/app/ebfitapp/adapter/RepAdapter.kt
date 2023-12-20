8943122176e3c1189da2eac86c1b44167a6dfe16
Berkan Taştekin <berkantastekinn@hotmail.com>
2023-12-20 22:09:50 +0300

    OneRm on the completion stage

--- app/src/main/java/com/app/ebfitapp/adapter/RepAdapter.kt
+++ app/src/main/java/com/app/ebfitapp/adapter/RepAdapter.kt
@@ -1,31 +1,62 @@
 package com.app.ebfitapp.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.app.ebfitapp.databinding.ItemRepBinding
+import kotlin.math.exp
 
-class RepAdapter(private val repList: List<Int>) : RecyclerView.Adapter<RepAdapter.RepViewHolder>() {
+class RepAdapter(private val repList: List<Int>, private val weight: Double,private val reps: Int) : RecyclerView.Adapter<RepAdapter.RepViewHolder>() {
 
-    class RepViewHolder(val bindingRep : ItemRepBinding) : RecyclerView.ViewHolder(bindingRep.root)
-    {
-
-    }
+    class RepViewHolder(val bindingRep: ItemRepBinding) : RecyclerView.ViewHolder(bindingRep.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepViewHolder {
-
-        val itemRepBinding = ItemRepBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        val itemRepBinding =
+            ItemRepBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return RepViewHolder(itemRepBinding)
-
     }
 
     override fun getItemCount(): Int {
         return repList.size
     }
 
-    override fun onBindViewHolder(holder: RepViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RepViewHolder, position: Int) = with(holder.bindingRep) {
+        val percentage = when (position) {
+            0 -> 100.0
+            1 -> 94.0
+            2 -> 91.0
+            3 -> 88.0
+            4 -> 86.0
+            5 -> 83.0
+            6 -> 81.0
+            7 -> 79.0
+            8 -> 77.0
+            9 -> 75.0
+            else -> 0.0
+        }
+
+        val oneRm = calculateOneRepMax(weight, reps)
+        val percentageOneRm = calculatePercentageOfOneRepMax(oneRm, percentage)
+
+        recyclerRmText.text = "${position+1}RM"
+        if(reps == position+1)
+        {
+            recyclerRmText.setTextColor(Color.RED)
+            recyclerWeightText.text = "$weight kg"
+        }else
+        {
+            recyclerRmText.setTextColor(Color.WHITE)
+            recyclerWeightText.text = String.format("%.2f kg", percentageOneRm)
+        }
 
     }
 
+    private fun calculateOneRepMax(weight: Double, repCount: Int): Double {
+        return 100.0 * weight / (48.8 + 53.8 * exp(-0.075 * repCount))
+    }
 
+    private fun calculatePercentageOfOneRepMax(oneRepMax: Double, percentage: Double): Double {
+        return oneRepMax * (percentage / 100.0)
+    }
 }
--- app/src/main/java/com/app/ebfitapp/fragment/OneRmFragment.kt
+++ app/src/main/java/com/app/ebfitapp/fragment/OneRmFragment.kt
@@ -1,18 +1,26 @@
 package com.app.ebfitapp.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.ebfitapp.R
+import com.app.ebfitapp.adapter.CalendarToDoAdapter
+import com.app.ebfitapp.adapter.RepAdapter
 import com.app.ebfitapp.databinding.FragmentCalculatorBinding
 import com.app.ebfitapp.databinding.FragmentOneRmBinding
 
 
 class OneRmFragment : Fragment() {
     private lateinit var fragmentOneRmFragmentBinding: FragmentOneRmBinding
+    private lateinit var repAdapter : RepAdapter
+    private val repList: List<Int> = (1..10).toList()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,12 +34,36 @@ class OneRmFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(fragmentOneRmFragmentBinding)
-        {
+        with(fragmentOneRmFragmentBinding) {
+
             goBackImage.setOnClickListener {
                 val goBackAction = OneRmFragmentDirections.actionOneRmFragmentToCalculatorFragment()
                 Navigation.findNavController(requireView()).navigate(goBackAction)
             }
+
+            val textWatcher = object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (weightText.text.isNotEmpty() && repsText.text.isNotEmpty()) {
+                        val weight = weightText.text.toString().toDouble()
+                        val reps = repsText.text.toString().toInt()
+
+
+                        repAdapter = RepAdapter(repList, weight,reps)
+                        repResultRecycler.layoutManager = LinearLayoutManager(this@OneRmFragment.requireContext())
+                        repResultRecycler.adapter = repAdapter
+                        repAdapter.notifyDataSetChanged()
+                        repResultRecycler.visibility = View.VISIBLE
+                    }
+                }
+            }
+            weightText.addTextChangedListener(textWatcher)
+            repsText.addTextChangedListener(textWatcher)
         }
     }
 
