1a7ad8ae8a6194092ea010e825ff79a986f53fc2
Berkan Taştekin <berkantastekinn@hotmail.com>
2023-12-31 12:59:31 +0300

    sa go bb forever

--- app/src/main/java/com/app/ebfitapp/fragment/RopeCalorieBurnedFragment.kt
+++ app/src/main/java/com/app/ebfitapp/fragment/RopeCalorieBurnedFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.app.ebfitapp.R
 import com.app.ebfitapp.databinding.FragmentRopeCalorieBurnedBinding
 
@@ -12,7 +13,9 @@ import com.app.ebfitapp.databinding.FragmentRopeCalorieBurnedBinding
 class RopeCalorieBurnedFragment : Fragment() {
 
     private lateinit var bindingRope : FragmentRopeCalorieBurnedBinding
-
+    private var weight : Double? = null
+    private var hours : Int? = null
+    private var minutes : Int? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +23,28 @@ class RopeCalorieBurnedFragment : Fragment() {
 
         bindingRope = FragmentRopeCalorieBurnedBinding.inflate(layoutInflater)
         return bindingRope.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(bindingRope){
+
+            goBackImage.setOnClickListener {
+                val goBackAction = RopeCalorieBurnedFragmentDirections.actionRopeCalorieBurnedFragmentToCalculatorFragment()
+                Navigation.findNavController(requireView()).navigate(goBackAction)
+            }
+            infoImage.setOnClickListener {
+                showRopeBurnedInfo()
+            }
+        }
+    }
+
+
+
+    private fun showRopeBurnedInfo(){
+
     }
 
 }
